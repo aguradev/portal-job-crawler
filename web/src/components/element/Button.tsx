48aa6cb6a9d77589ace6acb14264d2afc80a746e
aguradev <wahmayun.wm@gmail.com>
2024-05-25 21:32:37 +0800

    fix: changed to arrow function

--- web/src/components/element/Button.tsx
+++ web/src/components/element/Button.tsx
@@ -13,14 +13,14 @@ const Button: React.FC<ButtonProps> = ({
   clickHandler,
   classBtn,
 }) => {
-  function btnTypeSelected(isType?: string): string {
+  const btnTypeSelected = (isType?: string): string => {
     switch (isType) {
       case "primary":
         return "bg-blue-500 text-white";
       default:
         return "bg-slate-100 dark:bg-zinc-950/30 hover:bg-slate-200 dark:hover:bg-zinc-950/80";
     }
-  }
+  };
 
   return (
     <button
